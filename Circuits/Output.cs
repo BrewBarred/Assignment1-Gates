0b030f20e71b3b797fb0a67dcfbf46b12621e57b
Eli <elayjahh7@hotmail.com>
2023-09-13 15:23:07 +1200

    Writing draw method for input

--- Circuits/Output.cs
+++ Circuits/Output.cs
@@ -11,19 +11,22 @@
         protected int _outputWidth;
 
         //########## TEMPORARYILY ADDED TO STOP ERROR FROM INHERITING GATE WITHOUT A VALID CONSTRUCTOR #########
+        #region Output (int x, int y, int outputWidth) : base (x, y, outputWidth)
         public Output(int x, int y, int outputWidth) : base(x, y, outputWidth)
         {
             // sets gateWidth field to the passed gateWidth
-            outputWidth = outputWidth;
+            _outputWidth = outputWidth;
             // adds two input pins to the gate
             pins.Add(new Pin(this, true));
             pins.Add(new Pin(this, true));
-            // add an output pin to the gate
+            // adds an output pin to the gate
             pins.Add(new Pin(this, false));
             // move the gate and the pins to the position passed in
             MoveTo(x, y);
 
         } // end constructor
+        #endregion
+
     } // end class
 
 } // end namespace
